209cf88fef70e9b7835218bf4f96c01e26113573
Alberto Colombara <albertocolombara@hotmail.com>
2024-03-05 20:00:31 -0300

    feat: Implementação do filtro de categorias

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import Projeto from "./components/Projeto"
 import { projetos } from "./data/projetos";
 import Lenis from '@studio-freight/lenis'
 
 const App = () => {
+  const [filtro, setFiltro] = useState("Todos os projetos")
+
+  const projetosFiltrados = filtro === "Todos os projetos" ? projetos : projetos.filter(projeto => projeto.categoria === filtro)
+
+  const handleFiltro = (e) => {
+    setFiltro(e.target.textContent)
+  }
+
   useEffect(() => {
     const lenis = new Lenis({
       duration: 2,
@@ -30,14 +38,14 @@ const App = () => {
         <div className="mb-12 flex items-center justify-between">
           <h2 className="text-4xl font-astrid-light">Projetos</h2>
           <ul className="flex gap-5 text-sm">
-            <li className="cursor-pointer border border-neutral-600 bg-neutral-800 flex justify-center items-center rounded-full px-4 pt-3 pb-2 text-neutral-400 font-astrid-light hover:bg-neutral-800">TODOS OS PROJETOS</li>
-            <li className="cursor-pointer border border-neutral-600 flex justify-center items-center rounded-full px-4 pt-3 pb-2 text-neutral-400 font-astrid-light hover:bg-neutral-800">MODA</li>
-            <li className="cursor-pointer border border-neutral-600 flex justify-center items-center rounded-full px-4 pt-3 pb-2 text-neutral-400 font-astrid-light hover:bg-neutral-800">ESPORTES</li>
-            <li className="cursor-pointer border border-neutral-600 flex justify-center items-center rounded-full px-4 pt-3 pb-2 text-neutral-400 font-astrid-light hover:bg-neutral-800">BACKSTAGE</li>
+            <li onClick={handleFiltro} className={`cursor-pointer border border-neutral-600 ${filtro === "Todos os projetos" ? "bg-neutral-800" : ""} flex justify-center items-center rounded-full px-4 pt-3 pb-2 text-neutral-400 font-astrid-light hover:bg-neutral-800 uppercase`}>Todos os projetos</li>
+            <li onClick={handleFiltro} className={`cursor-pointer border border-neutral-600 ${filtro === "Moda" ? "bg-neutral-800" : ""} flex justify-center items-center rounded-full px-4 pt-3 pb-2 text-neutral-400 font-astrid-light hover:bg-neutral-800 uppercase`}>Moda</li>
+            <li onClick={handleFiltro} className={`cursor-pointer border border-neutral-600 ${filtro === "Esportes" ? "bg-neutral-800" : ""} flex justify-center items-center rounded-full px-4 pt-3 pb-2 text-neutral-400 font-astrid-light hover:bg-neutral-800 uppercase`}>Esportes</li>
+            <li onClick={handleFiltro} className={`cursor-pointer border border-neutral-600 ${filtro === "Backstage" ? "bg-neutral-800" : ""} flex justify-center items-center rounded-full px-4 pt-3 pb-2 text-neutral-400 font-astrid-light hover:bg-neutral-800 uppercase`}>Backstage</li>
           </ul>
         </div>
         <div className="grid grid-cols-4 grid-rows-[300px,300px,300px,300px] gap-y-[60px] gap-5">
-          {projetos.map((projeto, index) => {
+          {projetosFiltrados.map((projeto, index) => {
             return (
               <Projeto key={index} titulo={projeto.titulo} descricao={projeto.descricao} ano={projeto.ano} video={projeto.video} categoria={projeto.categoria} image={projeto.image} estilo={projeto.estilo} onhover={projeto.onHoverPreview} />
             )
