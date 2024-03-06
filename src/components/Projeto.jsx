9ad165ee5f6e25fab22f9942d62a55328ea2a245
Alberto Colombara <albertocolombara@hotmail.com>
2024-03-05 21:04:47 -0300

    feat: Ajuste da área de contato para fechar o overlay

--- src/components/Projeto.jsx
+++ src/components/Projeto.jsx
@@ -3,9 +3,17 @@ import { useState } from "react";
 const Projeto = ({ estilo, titulo, ano, categoria, image, onhover, descricao, video }) => {
     const [previewActive, setPreviewActive] = useState(false)
     const [overlayActive, setOverlayActive] = useState(false)
-
+    
     const handleOverlay = () => {
         setOverlayActive(!overlayActive)
+    }
+    
+    const handleCloseOverlay = (e) =>  {
+        const overlayId = document.querySelector("#overlay")
+        const overlayElement = overlayId.parentElement
+        if (e.target === overlayElement) {
+            setOverlayActive(!overlayActive)
+        }
     }
 
     return (
@@ -17,7 +25,7 @@ const Projeto = ({ estilo, titulo, ano, categoria, image, onhover, descricao, vi
                 </div>
                 <div className="h-full relative">
                     {image && 
-                        <img onMouseOver={() => setPreviewActive(true)} onMouseOut={() => setPreviewActive(false)} className="w-full h-full object-cover rounded-sm" src={ previewActive ? (onhover ?? image) : image}></img>
+                        <img alt={titulo + " Preview"} onMouseOver={() => setPreviewActive(true)} onMouseOut={() => setPreviewActive(false)} className={`w-full h-full object-cover rounded-sm transition-opacity duration-300 ${previewActive ? 'opacity-100' : 'opacity-70'}`} src={ previewActive ? (onhover ?? image) : image}></img>
                     }
                     {categoria && 
                         <span className="absolute bottom-2 right-2 bg-neutral-50 text-neutral-800 rounded-full font-astrid-light text-[12px] px-3 py-2 pb-1 uppercase">{categoria}</span>
@@ -25,12 +33,12 @@ const Projeto = ({ estilo, titulo, ano, categoria, image, onhover, descricao, vi
                 </div>
             </div>
             {overlayActive && 
-            <div className="bg-neutral-950/85 w-screen h-screen fixed top-0 left-0 z-40">
-                <div className="flex items-center justify-center flex-col gap-3 w-2/3 mx-auto absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <div className="bg-neutral-950/85 w-screen h-screen fixed top-0 left-0 z-40" onClick={handleCloseOverlay}>
+                <div id="overlay" className="flex items-center justify-center flex-col gap-3 w-2/3 h-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
                     <div className="w-full">
-                        <img className="w-full max-h-[600px] object-cover rounded-md" src={video ?? (onhover ?? image)}></img>
+                        <img className="w-full max-h-[60vh] object-cover rounded-md" src={video ?? (onhover ?? image)}></img>
                     </div>
-                    <div className="bg-neutral-950 px-8 py-12 rounded-md w-full flex justify-between gap-5">
+                    <div className="bg-neutral-950 p-8 rounded-md w-full flex justify-between gap-5">
                         <div className="flex flex-col flex-1">
                             <span className="text-3xl font-astrid-light text-neutral-50">{titulo}</span>
                             <span className="text-sm font-astrid-light text-neutral-400">{ano} // {categoria}</span>
@@ -38,7 +46,7 @@ const Projeto = ({ estilo, titulo, ano, categoria, image, onhover, descricao, vi
                         <span className="flex-[2] text-neutral-500">{descricao}</span>
                     </div>
                 </div>
-                <span className="absolute bottom-5 left-1/2 -translate-x-1/2 text-2xl cursor-pointer" onClick={handleOverlay}>Fechar</span>
+                {/* <span className="absolute bottom-5 left-1/2 -translate-x-1/2 text-2xl cursor-pointer" onClick={handleOverlay}>Fechar</span> */}
             </div>
             }
         </>
